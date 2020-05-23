a1b2d2573295bec34663b673174aa20a98b7d7ae
konyan <devkonyan@gmail.com>
2020-05-23 21:17:34 +0630

    add search input design

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const key = "da6c840b7ddc60d78c05ab45f9f4761b";
+
 export default async (lat, lon) => {
   const api = "https://fcc-weather-api.glitch.me/api/current?";
   const response = await axios.get(`${api + lat}&${lon}`);
   return response;
 };
+
+// api.openweathermap.org/data/2.5/weather?q="myanmar"&appid="da6c840b7ddc60d78c05ab45f9f4761b"
